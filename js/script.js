6b66583aa5f2632e10ea77845cad30cce5ea26b3
Karran Lemos <karranlemos@gmail.com>
2020-09-19 13:14:20 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+class Confusables {
+    constructor() {
+    }
+    static initAll() {
+        Helpers.initAll(Confusables, '.js-confusables');
+    }
+}
+class Helpers {
+    constructor() {
+        throw 'Static Only';
+    }
+    static initAll(classType, htmlClass) {
+        const elements = [];
+        const htmlElements = document.getElementsByClassName(htmlClass);
+        var i = 1;
+        for (const htmlElement of htmlElements) {
+            try {
+                elements.push(new classType(htmlElement));
+            }
+            catch (err) {
+                console.log(`${classType.name}[${i++}]: ${err}`);
+            }
+        }
+        return elements;
+    }
+}
+document.addEventListener('DOMContentLoaded', () => {
+    Confusables.initAll();
+});
+export {};
