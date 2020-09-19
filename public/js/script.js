cd57c77df4a9517c41f6e039a09c9fbfdea1c454
Karran Lemos <karranlemos@gmail.com>
2020-09-19 16:13:15 -0300

    Define basic class that represents the text editor

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,30 @@
 class Confusables {
-    constructor(confusables) {
+    constructor(confusable) {
+        this.verifyMembers = () => {
+            if (!this.percentageReplace)
+                throw "'.js-percentage-replace' not found...";
+            if (!this.input)
+                throw "'.js-input' not found...";
+            if (!this.output)
+                throw "'.js-output' not found...";
+            if (this.submitButtons.length === 0)
+                throw "'.js-submitButtons' not found...";
+        };
+        this.confusify = () => {
+        };
+        this.confusable = confusable;
+        this.percentageReplace = document.querySelector('.js-percentage-replace');
+        this.input = document.querySelector('.js-input');
+        this.output = document.querySelector('.js-output');
+        this.submitButtons = document.querySelectorAll('.js-submit');
+        try {
+            this.verifyMembers();
+        }
+        catch (err) {
+            throw err;
+        }
+        for (const submitButton of this.submitButtons)
+            submitButton.addEventListener('click', this.confusify);
     }
     static initAll() {
         Helpers.initAll(Confusables, '.js-confusables');
--- typescript/script.ts
+++ typescript/script.ts
@@ -1,9 +1,50 @@
 import { confusables } from './config/config';
 
 class Confusables {
-    constructor(confusables) {
 
+    private confusable: Element;
+    private percentageReplace: Element;
+    private input: Element;
+    private output: Element;
+    private submitButtons: NodeList;
+
+    constructor(confusable) {
+        this.confusable = confusable;
+
+        this.percentageReplace = document.querySelector('.js-percentage-replace');
+        this.input = document.querySelector('.js-input');
+        this.output = document.querySelector('.js-output');
+        this.submitButtons = document.querySelectorAll('.js-submit');
+
+        try {
+            this.verifyMembers();
+        }
+        catch (err) {
+            throw err;
+        }
+
+        for (const submitButton of this.submitButtons)
+            submitButton.addEventListener('click', this.confusify);
     }
+
+    private verifyMembers = () => {
+        if (!this.percentageReplace)
+            throw "'.js-percentage-replace' not found...";
+        if (!this.input)
+            throw "'.js-input' not found...";
+        if (!this.output)
+            throw "'.js-output' not found...";
+        if (this.submitButtons.length === 0)
+            throw "'.js-submitButtons' not found...";
+    };
+
+
+
+    private confusify = () => {
+        
+    };
+
+    
 
     static initAll() {
         Helpers.initAll(Confusables, '.js-confusables');
